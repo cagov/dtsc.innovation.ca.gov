78aa09b04bc708e7dbebd0bae1cdbce0dea0d9c9
Aaron Hans <aaronhans@gmail.com>
2022-10-31 16:32:50 -0700

    v1

---
+++ readme.md
@@ -0,0 +1,1 @@
+new domains and certs being requested here: https://services.cdt.ca.gov/csm?id=csm_ticket&is_new_order=true&table=sn_customerservice_case&sys_id=40ee51421bbe9914ad438557ec4bcb79
---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+var map = L.map('map').setView([34.1123495573206, -117.31944347283586], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
