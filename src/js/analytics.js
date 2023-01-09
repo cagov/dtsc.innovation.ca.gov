cafd9eec5800905681d7a2beba377a092998bcf1
Michael Sullivan <69169190+m-sullivan7@users.noreply.github.com>
2023-01-09 13:26:16 -0800

    Update Meta pixel
    One value was different when I compared the code I see in Facebook Events Manager to what's in this file. I suspect updating this is a first step before we do the event code.

--- src/js/analytics.js
+++ src/js/analytics.js
@@ -43,5 +43,5 @@ n.queue=[];t=b.createElement(e);t.async=!0;
 t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '1197520694174921');
+fbq('init', '1171913563402571');
 fbq('track', 'PageView');
